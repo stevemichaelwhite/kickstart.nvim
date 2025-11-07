0551934e03ddf9110f19a9fdcd5cb40768557c29
Steve White <steve_white@trimble.com>
2025-11-08 11:47:40 +1300

    Removed duplicate treesitter definition

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -264,18 +264,6 @@ return {
     {
         '3rd/image.nvim',
         event = 'VeryLazy',
-        dependencies = {
-            {
-                'nvim-treesitter/nvim-treesitter',
-                build = ':TSUpdate',
-                config = function()
-                    require('nvim-treesitter.configs').setup {
-                        ensure_installed = { 'markdown' },
-                        highlight = { enable = true },
-                    }
-                end,
-            },
-        },
         opts = {
             backend = 'kitty',
             integrations = {
